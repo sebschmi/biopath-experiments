e7d54c02ec6341853cb21ed8739e53868e08f66f
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2026-02-25 09:47:04 +0200

    Create run script.

--- run.sh
+++ run.sh
@@ -2,12 +2,23 @@
 
 set +e
 
+echo "Checking for running snakemake"
+ps ux | grep -F "bin/snakemake " | grep -v grep
+if [ "${PIPESTATUS[2]}" -ne "0" ]; then
+    echo "No snakemake found"
+else
+    echo "Snakemake is already running!"
+    exit 1
+fi
+
+set -e
+
 HOSTNAME = $(hostname)
 
 if [[ $HOSTNAME == "dx3"* ]]; then
     echo "Running on dx3, using profile dx3"
-    snakemake --profile profiles/dx3
+    nohup snakemake --profile profiles/dx3 "$@" > current.log 2>&1 &
 else
     echo "Running on unknown host $HOSTNAME, using default profile"
-    snakemake
+    nohup snakemake "$@" > current.log 2>&1 &
 fi
